4c2a51877ae65aa9e0b028e4e2bfe204be0ace94
MikhailKlimentyev <mikhail.klimentiev@gmail.com>
2018-02-11 21:09:49 +0300

    Added constructors to OperationsSet.java and OperationSwitcher.java, Calculator.java has been renamed to OperationsSet.java

---
+++ part_1/src/main/java/tasks/matrix/RunnerMatrix.java
@@ -0,0 +1,28 @@
+package tasks.matrix;
+
+import java.util.Scanner;
+
+/**
+ * RunnerMatrix.
+ *
+ * @author Mikhail Klimentsyeu
+ * @version 1.0
+ * @since 02/11/2018
+ */
+public class RunnerMatrix {
+	public static void main(String [] args){
+		Scanner scan = new Scanner(System.in);
+
+		Matrix matrix = new Matrix();
+
+		int size = matrix.getSizeOfSquareMatrix(scan);
+
+		matrix.setMatrixArray(new int[size][size]);
+
+		matrix.getMmatrixElements(matrix.getMatrixArray(), scan);
+
+		matrix.showMatrix(matrix.getMatrixArray());
+
+		matrix.showMatrix(matrix.rotateMatrix(matrix.getMatrixArray()));
+	}
+}
---
+++ part_1/src/main/java/tasks/matrix/package-info.java
@@ -0,0 +1,7 @@
+package tasks.matrix;
+
+/**
+ * @author Mikhail Klimentsyeu
+ * @version 1.0
+ * @since 02/11/2018
+ */
