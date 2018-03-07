30d07f53ff781ac09d3633ca122883665d62b524
MikhailKlimentyev <mikhail.klimentiev@gmail.com>
2018-03-08 01:12:12 +0300

    Sedan's objects created

--- part_2/src/main/java/tasks/model/Runner.java
+++ part_2/src/main/java/tasks/model/Runner.java
@@ -10,36 +10,38 @@ public class Runner {
 
 		Scanner scan = new Scanner(System.in);
 
-		Vehicle bmw3Series = new Sedan("Sedan","BMW 3 series", "Automatic", 8.71,
+		//Sedan's objects
+		Vehicle bmw3Series = new Sedan("Sedan","BMW 3 series", "Automatic", 8.00,
 				40250, 5, 2013);
-		Vehicle bmw4Series = new Sedan("Sedan","BMW 3 series", "Automatic", 8.70,
-				40350, 5, 2015);
-		bmw3Series.getNumberOfSeats();
-		bmw3Series.getLitresPerHudredKm();
-		bmw3Series.getTransmission();
-		bmw3Series.getModel();
-		bmw3Series.getCost();
-		bmw3Series.getType();
-		bmw3Series.inform();
-		bmw3Series.drive();
+		Vehicle bmw4Series = new Sedan("Sedan","BMW 4 Series Gran Coupe", "Automatic",
+				8.71, 43300, 5, 2015);
+		Vehicle jaguar = new Sedan("Sedan", "Jaguar X-Type", "Automatic", 13.07,
+				35060, 5, 2008);
+		Vehicle mercedesBenzC43 = new Sedan("Sedan", "Mercedes-Benz C43", "Manual",
+				12.0, 10000, 5, 2010);
+
 
 		List<Vehicle> autopark = new ArrayList();
 		autopark.add(bmw3Series);
 		autopark.add(bmw4Series);
+		autopark.add(jaguar);
+		autopark.add(mercedesBenzC43);
+
+
 
 //		TaxiStation taxiStation = new TaxiStation();
 //		taxiStation.searchCarByParameters(autopark, scan);
-
-		TaxiStation taxiStation = new TaxiStation();
-		taxiStation.calculateCost(autopark);
-
+//
+//		TaxiStation taxiStation = new TaxiStation();
+//		taxiStation.calculateCost(autopark);
+//
 //		Collections.sort(autopark, new FuelConsumptionComparator());
 //		System.out.println("Order of cars in the Taxi Station on fuel consumption :");
 //		for (Vehicle car : autopark) {
 //			car.drive();
 //		}
-
-		taxiStation.printAllCars(autopark);
+//
+//		taxiStation.printAllCars(autopark);
 
 
 
