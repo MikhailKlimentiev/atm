a27c6fef43d235b99d72b64b8a3a95d307435fa6
MikhailKlimentyev <mikhail.klimentiev@gmail.com>
2018-03-11 22:36:20 +0300

    added comments

--- part_2/src/main/java/tasks/model/Runner.java
+++ part_2/src/main/java/tasks/model/Runner.java
@@ -20,7 +20,7 @@ public class Runner {
 		List<Vehicle> taxiStation = new ArrayList();
 		TaxiStationOperations taxiStationOperations = new TaxiStationOperations();
 
-		//Sedan's objects creation
+//		Sedan's objects creation
 		Sedan bmw3Series = new Sedan("Sedan", "BMW 3 series", "Automatic", 8.00,
 				40250, 5, 2013);
 		Sedan bmw4Series = new Sedan("Sedan", "BMW 4 Series Gran Coupe", "Automatic",
@@ -30,25 +30,25 @@ public class Runner {
 		Sedan mercedesBenzC43 = new Sedan("Sedan", "Mercedes-Benz C43", "Manual",
 				12.0, 10000, 5, 2010);
 
-		//Cabriolet's objects creation
+//		Cabriolet's objects creation
 		Cabriolet audiA3Automatic = new Cabriolet("Cabriolet", "Audi A3 Convertible", "Automatic",
 				9.41, 38250, 4, 2018, false);
 		Cabriolet audiA3Manual = new Cabriolet("Cabriolet", "Audi A3 Convertible", "Manual",
 				9.41, 35300, 4, 2018, true);
 
-		//Crossover's objects creation
+//		Crossover's objects creation
 		Crossover jeepPatriot = new Crossover("Crossover", "Jeep Patriot 4dr", "Automatic",
 				10.69, 25840, 5, 2017);
 		Crossover vwTiguan = new Crossover("Crossover", "Volkswagen Tiguan Limited 4dr", "Manual",
 				11, 21995, 5, 2017);
 
-		//Truck's objects creation
+//		Truck's objects creation
 		Truck chevroletColorado15 = new Truck("Truck", "Chevrolet Colorado Crew Cab Pickup",
 				"Automatic", 12.38, 35780, 5, 2015);
 		Truck chevroletColorado16 = new Truck("Truck", "Chevrolet Colorado Crew Cab Pickup",
 				"Automatic", 12.38, 35780, 5, 2016);
 
-		//adding cars to autopark
+//		adding cars to autopark
 		taxiStation.add(bmw3Series);
 		taxiStation.add(bmw4Series);
 		taxiStation.add(jaguar);
@@ -60,16 +60,16 @@ public class Runner {
 		taxiStation.add(chevroletColorado15);
 		taxiStation.add(chevroletColorado16);
 
-		//print all cars in the Taxi Station
+//		print all cars in the Taxi Station
 		taxiStationOperations.printAllCars(taxiStation);
 
-		//calculate cost of all cars of the Taxi Station
+//		calculate cost of all cars of the Taxi Station
 		taxiStationOperations.calculateCost(taxiStation);
 
-		//search a car by parameters in the Taxi Station
+//		search a car by parameters in the Taxi Station
 		taxiStationOperations.searchCarByParameters(taxiStation, scan);
 
-		//sort cars of the Taxi Station by fuel consumption
+//		sort cars of the Taxi Station by fuel consumption
 		Collections.sort(taxiStation, new FuelConsumptionComparator());
 		System.out.println("\nOrder of cars in the Taxi Station on fuel consumption:");
 		int count = 0;
