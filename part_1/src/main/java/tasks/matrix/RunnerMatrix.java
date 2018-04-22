eca9fda8ad7bba1a55f4bfb5a073e55b229a2066
MikhailKlimentyev <mikhail.klimentiev@gmail.com>
2018-04-22 14:09:31 +0300

    Refactoring

--- part_1/src/main/java/tasks/matrix/RunnerMatrix.java
+++ part_1/src/main/java/tasks/matrix/RunnerMatrix.java
@@ -1,4 +1,18 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////                                                                                                           ////
+////    PRE-SELECTION FOR TEST AUTOMATION MENTORING PARTICIPATION: JAVA, part 1                                ////
+////                                                                                                           ////
+////    2. Решить алгоритмическую задачу (варианты «загуглить» крайне не приветствуются).                     ////
+////    Варианты ниже (вариант необходимо выбрать самостоятельно):                                             ////
+////                                                                                                           ////
+////   Turn a matrix on 90 degrees clockwise (Повернуть матрицу на 90 градусов по часовой стрелке)             ////
+////                                                                                                           ////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 package tasks.matrix;
+
+import tasks.matrix.Matrix;
 
 import java.util.Scanner;
 
@@ -10,19 +24,19 @@ import java.util.Scanner;
  * @since 02/11/2018
  */
 public class RunnerMatrix {
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
 
-		Matrix matrix = new Matrix();
+        Matrix matrix = new Matrix();
 
-		int size = matrix.getSizeOfSquareMatrix(scan);
+        int size = matrix.getSizeOfSquareMatrix(scan);
 
-		matrix.setMatrixArray(new int[size][size]);
+        matrix.setMatrixArray(new int[size][size]);
 
-		matrix.getMmatrixElements(matrix.getMatrixArray(), scan);
+        matrix.getMmatrixElements(matrix.getMatrixArray(), scan);
 
-		matrix.showMatrix(matrix.getMatrixArray());
+        matrix.showMatrix(matrix.getMatrixArray());
 
-		matrix.showMatrix(matrix.rotateMatrix(matrix.getMatrixArray()));
-	}
+        matrix.showMatrix(matrix.rotateMatrix(matrix.getMatrixArray()));
+    }
 }
--- part_1/src/main/java/tasks/matrix/package-info.java
+++ part_1/src/main/java/tasks/matrix/package-info.java
@@ -1,7 +1,7 @@
-package tasks.matrix;
-
 /**
  * @author Mikhail Klimentsyeu
  * @version 1.0
  * @since 02/11/2018
  */
+
+package tasks.matrix;
